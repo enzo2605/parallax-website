c5b73f9b8be5b0b743cd5a1849faf82718c2e119
enzo2605 <34918941+enzo2605@users.noreply.github.com>
2020-03-01 13:00:36 +0100

    Finally, the slider work.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,61 @@
-let slides, index = 0, btnRight;
+let slides, index = 0, btnRight, btnLeft;
+
 slides = document.querySelectorAll('.slide');
+btnRight = document.querySelector('.arrow-right');
+btnLeft = document.querySelector('.arrow-left');
 
-function slider() {
-    index++;
+function sliderRight() {
 
-    if(index == slides.length) 
-        index = 1;
+    let index = parseInt(btnRight.getAttribute('id'));
+    console.log('RIGHT: '+index);
 
-    slides[index].style.display = 'block';
+    //se l'indice è uguale alla lunghezza dell'array slides.
+    if(index == slides.length-1) {
+        //rimuovi la classe 'active' dall'elemento corrente.
+        slides[index].classList.remove('active');
+        //resetta l'indice
+        index = 0;
+        slides[index].classList.add('active');
+        btnRight.setAttribute('id',index);
+    }
+    
+    //se l'indice è diverso da 0, rimuovi la classe 'active' dall'elemento precedente.
+    else {
+        //rimuovi la classe 'active' dall'elemento corrente.
+        slides[index].classList.remove('active'); 
+        //aggiungo la classe active all'elemento successivo.
+        slides[index+1].classList.add('active');
+        //aggiorno l'id del bottone destro con il numero della diapositiva corrente che sto visualizzando
+        btnRight.setAttribute('id',index+1);
+    }
 }
 
-btnRight = document.querySelector('.arrow');
+function sliderLeft() {
 
-btnRight.addEventListener('click', function () {
-    slider();
+    console.log('LEFT: '+btnRight.getAttribute('id'));
+
+    //leggo il valore dell'id del bottone destro
+    let i = btnRight.getAttribute('id');
+
+    if(i == "0") {
+        slides[i].classList.remove('active');
+        slides[slides.length - 1].classList.add('active');
+        //aggiorno l'id del bottone
+        btnRight.setAttribute('id',slides.length - 1);
+    }
+
+    else {
+        slides[i].classList.remove('active');
+        slides[i-1].classList.add('active');
+        //aggiorno l'id del bottone
+        btnRight.setAttribute('id',i-1);
+    }
+}
+
+btnRight.addEventListener('click', () => {
+    sliderRight();
+}, false);
+
+btnLeft.addEventListener('click', () => {
+    sliderLeft();
 }, false);
